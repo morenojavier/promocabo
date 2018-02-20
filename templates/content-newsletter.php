d647a1a009acac32b79d6e2393a900a9a835b24f
Javier Moreno <javier.moreno@basiko.mx>
2018-02-19 21:07:13 -0600

    [add] Inicio de proyecto

---
+++ templates/content-newsletter.php
@@ -0,0 +1,36 @@
+<div class="container newsletter-bg">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<!-- Begin MailChimp Signup Form -->
+			<div class="newsletter-contentform mg-top-30">
+				<div class="newsletter-contentform-logo">
+					<img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo_circulo_194.png" alt="" class="img-responsive">
+				</div>
+				<div id="mc_embed_signup">
+					<div class="newsletter-contentform-introimg mg-bottom-20">
+						<h4>¿Estás listo? Dejanos tu correo y te compartiremos nuestro catálogo de servicios en cuanto lo tengamos listo <img src="<?php echo get_template_directory_uri(); ?>/dist/images/map-marker-small.png" alt=""></h4>
+						<img src="<?php echo get_template_directory_uri(); ?>/dist/images/icon_mailing.png" alt="">
+					</div>
+					<form action="https://promocabo.us17.list-manage.com/subscribe/post?u=e09ddac03ab99db0e71592965&amp;id=45dbbe6651" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<div id="mc_embed_signup_scroll">
+							<div class="mc-field-group">
+
+								<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="E-mail">
+							</div>
+							
+							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e09ddac03ab99db0e71592965_45dbbe6651" tabindex="-1" value=""></div>
+							<div class="clear"><input type="submit" value="&#xf1d8" name="subscribe" id="mc-embedded-subscribe" class="newsletter-button"></div>
+						</div>
+
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					</form>
+				</div>
+				<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+				<!--End mc_embed_signup-->
+			</div>
+		</div>
+	</div>
+</div>
